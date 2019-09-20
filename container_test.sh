e56603c4aa57edd5cc85e4b0de95ef0f3b168284
RJ Souza <rjsouza1995@gmail.com>
2019-09-19 20:45:55 -0700

    Update container_test.sh

--- container_test.sh
+++ container_test.sh
@@ -4,11 +4,11 @@ set -e
 backendrun=$(docker inspect -f {{.State.Running}} backend-demo-app)
 if [ "$backendrun" = "false" ]; then
    echo "backend-demo-app down, Exiting..."
-   exit 1 
+   exit 0 
 fi
 
 frontrun=$(docker inspect -f {{.State.Running}} frontend-demo-app)
 if [ "$frontend" = "false" ]; then
    echo "frontend-demo-app down, Exiting..."
-   exit 1 
+   exit 0 
 fi
